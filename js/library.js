6fa150fc02e5a1b96fef4420872dd3ac48534a99
Chris <info@cu-tec.de>
2014-04-06 12:55:15 +0200

    GUID function

--- js/library.js
+++ js/library.js
@@ -59,3 +59,12 @@ function getCookie(Name){
   }
   return c_value;
 }
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+               .toString(16)
+               .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+         s4() + '-' + s4() + s4() + s4();
+}
