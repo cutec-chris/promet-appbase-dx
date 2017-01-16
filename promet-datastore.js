a47822bcec2977d5f2960f396c58eb044b2d24e4
Chris <ulrich@tcs-germany.de>
2017-01-16 15:48:32 +0100

    Datastore:started implementing updating

--- promet-datastore.js
+++ promet-datastore.js
@@ -1,5 +1,17 @@
-function newPrometDataStore() {
+function newPrometDataStore(aName,aSheme) {
   var aDS = new dhtmlXDataStore({ datatype:"json" });
-
+  aDS.TableName = aName;
+  if (aSheme) {
+    aDS.data.sheme(aSheme);
+  }
+  aDS.attachEvent("onAfterAdd", function(id) {
+    console.log('new Record');
+  });
+  aDS.attachEvent("onAfterDelete",function(id) {
+    console.log('deleted Record');
+  });
+  aDS.attachEvent("onStoreUpdated", function(id, data, mode){
+    console.log('updated Record');
+  });
   return aDS;
 }
