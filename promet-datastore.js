22def12c2770cda9440c1bf6e2af726c4e7ac8dd
Chris <ulrich@tcs-germany.de>
2017-01-17 15:40:58 +0100

    trial

--- promet-datastore.js
+++ promet-datastore.js
@@ -4,14 +4,22 @@ function newPrometDataStore(aName,aSheme) {
   if (aSheme) {
     aDS.data.sheme(aSheme);
   }
+  /*
   aDS.attachEvent("onAfterAdd", function(id) {
     console.log('new Record');
   });
   aDS.attachEvent("onAfterDelete",function(id) {
     console.log('deleted Record');
   });
+  */
   aDS.attachEvent("onStoreUpdated", function(id, data, mode){
     console.log('updated Record');
+  });
+  aDS.attachEvent("onDataRequest", function(start, count){
+    console.log('Data Request');
+  });
+  aDS.attachEvent("onAfterCursorChange", function(id){
+    console.log('Cursor changed');
   });
   return aDS;
 }
