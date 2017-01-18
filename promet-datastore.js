61c52b67deee723cd1665b69b8ccfc4c1db28e54
Chris <ulrich@tcs-germany.de>
2017-01-18 14:25:58 +0100

    Add Datastore and Dataprocessor

--- promet-datastore.js
+++ promet-datastore.js
@@ -1,20 +1,21 @@
 function newPrometDataStore(aName,aSheme) {
-  var aDS = new dataProcessor(aName);
+  var aDS = new dhtmlXDataStore();
+  aDS.DataProcessor = new dataProcessor(aName);
   aDS.TableName = aName;
   if (aSheme) {
     aDS.data.sheme(aSheme);
   }
-  aDS.setTransactionMode("REST",false);
-  aDS.enablePartialDataSend(true);
-  aDS.attachEvent("onBeforeDataSending", function(id, mode, data){
+  aDS.DataProcessor.setTransactionMode("REST",false);
+  aDS.DataProcessor.enablePartialDataSend(true);
+  aDS.DataProcessor.attachEvent("onBeforeDataSending", function(id, mode, data){
     //here you can place your own data saving logic
     console.log(aDS.TableName,'data should be send ',id,mode,data);
     return false;
   });
-  aDS.attachEvent("onAfterUpdateFinish",function(){
+  aDS.DataProcessor.attachEvent("onAfterUpdateFinish",function(){
      alert(aDS.TableName,"single row updated")
   });
-  aDS.attachEvent("onFullSync",function(){
+  aDS.DataProcessor.attachEvent("onFullSync",function(){
      alert(aDS.TableName,"all rows updated")
   });
   return aDS;
