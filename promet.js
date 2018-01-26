4f25d80ae156b2e8f97f23d6dd5ad49fb77d0019
Chris <ulrich@tcs-germany.de>
2018-01-26 15:45:09 +0100

    Remove Function for Arrays

--- promet.js
+++ promet.js
@@ -273,3 +273,9 @@ pad = function(n, width, z) {
   n = n + '';
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
+
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
