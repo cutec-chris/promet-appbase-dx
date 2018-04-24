77a99d2a831c33698c244eaedd66dddbaf514533
Chris <ulrich@tcs-germany.de>
2018-04-24 12:55:13 +0200

    Working PDF Viewer (Text selection dont work at time)

--- js/avammpdf.js
+++ js/avammpdf.js
@@ -1,77 +1,74 @@
 var scale = 1.5; //Set this to whatever you want. This is basically the "zoom" factor for the PDF.
+var PDFJS = window['pdfjs-dist/build/pdf'];
 
 function loadPdf(pdfData) {
     PDFJS.disableWorker = true; //Not using web workers. Not disabling results in an error. This line is
     //missing in the example code for rendering a pdf.
-
-    var pdf = PDFJS.getDocument(pdfData);
-    pdf.then(renderPdf);
-    return pdf;
+    // The workerSrc property shall be specified.
+    //PDFJS.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+    var loadingTask = PDFJS.getDocument(pdfData);
+    loadingTask.promise.then(function(pdf) {
+      console.log('PDF loaded');
+      renderPdf(pdf);
+      return pdf;
+    });  
 }
 
 function renderPdf(pdf) {
   pdf.getPage(1).then(renderPage);
 }
 
-    function renderPage(page) {
-        var viewport = page.getViewport(scale);
-        var $canvas = jQuery("<canvas></canvas>");
+function renderPage(page) {
+    var viewport = page.getViewport(scale);
+    var $canvas = jQuery("<canvas></canvas>");
+    //Set the canvas height and width to the height and width of the viewport
+    var canvas = $canvas.get(0);
+    var context = canvas.getContext("2d");
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+    //Append the canvas to the pdf container div
+    var $pdfContainer = jQuery("#pdfContainer");
+    $pdfContainer.css("height", canvas.height + "px").css("width", canvas.width + "px");
+    $pdfContainer.append($canvas);
+    //The following few lines of code set up scaling on the context if we are on a HiDPI display
+    var outputScale = getOutputScale();
+    if (outputScale.scaled) {
+        var cssScale = 'scale(' + (1 / outputScale.sx) + ', ' +
+            (1 / outputScale.sy) + ')';
+        CustomStyle.setProp('transform', canvas, cssScale);
+        CustomStyle.setProp('transformOrigin', canvas, '0% 0%');
 
-        //Set the canvas height and width to the height and width of the viewport
-        var canvas = $canvas.get(0);
-        var context = canvas.getContext("2d");
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-
-        //Append the canvas to the pdf container div
-        var $pdfContainer = jQuery("#pdfContainer");
-        $pdfContainer.css("height", canvas.height + "px").css("width", canvas.width + "px");
-        $pdfContainer.append($canvas);
-
-        //The following few lines of code set up scaling on the context if we are on a HiDPI display
-        var outputScale = getOutputScale();
-        if (outputScale.scaled) {
-            var cssScale = 'scale(' + (1 / outputScale.sx) + ', ' +
-                (1 / outputScale.sy) + ')';
-            CustomStyle.setProp('transform', canvas, cssScale);
-            CustomStyle.setProp('transformOrigin', canvas, '0% 0%');
-
-            if ($textLayerDiv.get(0)) {
-                CustomStyle.setProp('transform', $textLayerDiv.get(0), cssScale);
-                CustomStyle.setProp('transformOrigin', $textLayerDiv.get(0), '0% 0%');
-            }
+        if ($textLayerDiv.get(0)) {
+            CustomStyle.setProp('transform', $textLayerDiv.get(0), cssScale);
+            CustomStyle.setProp('transformOrigin', $textLayerDiv.get(0), '0% 0%');
         }
-
-        context._scaleX = outputScale.sx;
-        context._scaleY = outputScale.sy;
-        if (outputScale.scaled) {
-            context.scale(outputScale.sx, outputScale.sy);
-        }
-
-        var canvasOffset = $canvas.offset();
-        var $textLayerDiv = jQuery("<div />")
-            .addClass("textLayer")
-            .css("height", viewport.height + "px")
-            .css("width", viewport.width + "px")
-            .offset({
-                top: canvasOffset.top,
-                left: canvasOffset.left
-            });
-
-        $pdfContainer.append($textLayerDiv);
-
-        page.getTextContent().then(function (textContent) {
-            var textLayer = new TextLayerBuilder($textLayerDiv.get(0), 0); //The second zero is an index identifying
-            //the page. It is set to page.number - 1.
-            textLayer.setTextContent(textContent);
-
-            var renderContext = {
-                canvasContext: context,
-                viewport: viewport,
-                textLayer: textLayer
-            };
-
-            page.render(renderContext);
-        });
     }
+    context._scaleX = outputScale.sx;
+    context._scaleY = outputScale.sy;
+    if (outputScale.scaled) {
+        context.scale(outputScale.sx, outputScale.sy);
+    }
+
+    var canvasOffset = $canvas.offset();
+    var $textLayerDiv = jQuery("<div />")
+        .addClass("textLayer")
+        .css("height", viewport.height + "px")
+        .css("width", viewport.width + "px")
+        .offset({
+            top: canvasOffset.top,
+            left: canvasOffset.left
+        });
+    $pdfContainer.append($textLayerDiv);
+    page.getTextContent().then(function (textContent) {
+        var textLayer = new TextLayerBuilder($textLayerDiv.get(0), 0); //The second zero is an index identifying
+        //the page. It is set to page.number - 1.
+        textLayer.setTextContent(textContent);
+        var renderContext = {
+            canvasContext: context,
+            viewport: viewport,
+            textLayer: textLayer
+        };
+        page.render(renderContext);
+    });
+}
 
