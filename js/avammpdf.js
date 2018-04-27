aec3a03a76ef84adcc91e55f3c4706303e6b5ac6
Chris <ulrich@tcs-germany.de>
2018-04-27 08:12:30 +0200

    PDF Viewer: remove jQuerry dependency Render multiple Pages

--- js/avammpdf.js
+++ js/avammpdf.js
@@ -1,5 +1,7 @@
 var scale = 1.5; //Set this to whatever you want. This is basically the "zoom" factor for the PDF.
 var PDFJS = window['pdfjs-dist/build/pdf'];
+var currPage = 1;
+var thePdf;
 
 function loadPdf(pdfData) {
     PDFJS.disableWorker = true; //Not using web workers. Not disabling results in an error. This line is
@@ -15,60 +17,82 @@ function loadPdf(pdfData) {
 }
 
 function renderPdf(pdf) {
-  pdf.getPage(1).then(renderPage);
+  document.body.innerHTML = "";
+  pdf.getPage( 1 ).then( handlePages );
+  thePdf = pdf;
 }
 
-function renderPage(page) {
-    var viewport = page.getViewport(scale);
-    var $canvas = jQuery("<canvas></canvas>");
-    //Set the canvas height and width to the height and width of the viewport
-    var canvas = $canvas.get(0);
-    var context = canvas.getContext("2d");
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-    //Append the canvas to the pdf container div
-    var $pdfContainer = jQuery("#pdfContainer");
-    $pdfContainer.css("height", canvas.height + "px").css("width", canvas.width + "px");
-    $pdfContainer.append($canvas);
-    //The following few lines of code set up scaling on the context if we are on a HiDPI display
-    var outputScale = getOutputScale();
-    if (outputScale.scaled) {
-        var cssScale = 'scale(' + (1 / outputScale.sx) + ', ' +
-            (1 / outputScale.sy) + ')';
-        CustomStyle.setProp('transform', canvas, cssScale);
-        CustomStyle.setProp('transformOrigin', canvas, '0% 0%');
+function handlePages(page)
+{
+  //This gives us the page's dimensions at full scale
+  var viewport = page.getViewport( scale );
 
-        if ($textLayerDiv.get(0)) {
-            CustomStyle.setProp('transform', $textLayerDiv.get(0), cssScale);
-            CustomStyle.setProp('transformOrigin', $textLayerDiv.get(0), '0% 0%');
-        }
-    }
-    context._scaleX = outputScale.sx;
-    context._scaleY = outputScale.sy;
-    if (outputScale.scaled) {
-        context.scale(outputScale.sx, outputScale.sy);
-    }
+  var container = document.createElement( "div" );
+  
+  document.body.appendChild(container);
+  container.className = 'pdf-content';
 
-    var canvasOffset = $canvas.offset();
-    var $textLayerDiv = jQuery("<div />")
-        .addClass("textLayer")
-        .css("height", viewport.height + "px")
-        .css("width", viewport.width + "px")
-        .offset({
-            top: canvasOffset.top,
-            left: canvasOffset.left
-        });
-    $pdfContainer.append($textLayerDiv);
-    page.getTextContent().then(function (textContent) {
-        var textLayer = new TextLayerBuilder($textLayerDiv.get(0), 0); //The second zero is an index identifying
-        //the page. It is set to page.number - 1.
-        textLayer.setTextContent(textContent);
-        var renderContext = {
-            canvasContext: context,
-            viewport: viewport,
-            textLayer: textLayer
-        };
-        page.render(renderContext);
-    });
+  //We'll create a canvas for each page to draw it on
+  var canvas = document.createElement( "canvas" );
+  canvas.style.display = "block";
+  var context = canvas.getContext('2d');
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+
+  //Draw it on the canvas
+  //page.render({canvasContext: context, viewport: viewport});
+  container.style.height = canvas.height;
+  container.style.width = canvas.width;
+
+  var outputScale = getOutputScale();
+  if (outputScale.scaled) {
+      var cssScale = 'scale(' + (1 / outputScale.sx) + ', ' +
+          (1 / outputScale.sy) + ')';
+      CustomStyle.setProp('transform', canvas, cssScale);
+      CustomStyle.setProp('transformOrigin', canvas, '0% 0%');
+
+      if ($textLayerDiv.get(0)) {
+          CustomStyle.setProp('transform', $textLayerDiv.get(0), cssScale);
+          CustomStyle.setProp('transformOrigin', $textLayerDiv.get(0), '0% 0%');
+      }
+  }
+  context._scaleX = outputScale.sx;
+  context._scaleY = outputScale.sy;
+  if (outputScale.scaled) {
+      context.scale(outputScale.sx, outputScale.sy);
+  }
+
+  var canvasOffset = {
+      top: canvas.getBoundingClientRect().top + document.body.scrollTop,
+      left: canvas.getBoundingClientRect().left + document.body.scrollLeft
+  };
+
+  var textLayerDiv =  document.createElement( "div" );
+  textLayerDiv.className = "textLayer";
+  textLayerDiv.style.height = viewport.height;
+  textLayerDiv.style.width = viewport.width;
+  textLayerDiv.style.top = canvasOffset.top;
+  textLayerDiv.style.left = canvasOffset.left;
+  container.append(textLayerDiv);
+  page.getTextContent().then(function (textContent) {
+      var textLayer = new TextLayerBuilder(textLayerDiv[0], 0); //The second zero is an index identifying
+      //the page. It is set to page.number - 1.
+      textLayer.setTextContent(textContent);
+      var renderContext = {
+          canvasContext: context,
+          viewport: viewport,
+          textLayer: textLayer
+      };
+      page.render(renderContext);
+  });
+
+  //Add it to the web page
+  container.appendChild( canvas );
+
+  //Move to next page
+  currPage++;
+  if ( thePdf !== null && currPage <= thePdf.numPages )
+  {
+     thePdf.getPage( currPage ).then( handlePages );
+  }
 }
-
