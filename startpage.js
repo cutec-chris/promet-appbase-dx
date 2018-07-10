bd1325f105d506dc1bfac76111ee5a0734ac6b36
Chris <ulrich@tcs-germany.de>
2018-07-10 13:40:35 +0200

    Make Startpage to separate Project

---
+++ startpage.js
@@ -0,0 +1,19 @@
+﻿rtl.module("startpage",["System","JS","Classes","Avamm","webrouter","dhtmlx_base","SysUtils","AvammWiki"],function () {
+  "use strict";
+  var $mod = this;
+  this.DoShowStartpage = function (URl, aRoute, Params) {
+    pas.AvammWiki.ShowStartpage();
+  };
+  this.LoadStartpage = function (aValue) {
+    var Result = undefined;
+    if (pas.webrouter.Router().FindHTTPRoute("startpage",null) !== null) return Result;
+    pas.Avamm.RegisterSidebarRoute(rtl.getResStr(pas.startpage,"strStartpage"),"startpage",$mod.DoShowStartpage);
+    if (pas.webrouter.Router().GetHistory().$class.getHash() === "") pas.webrouter.Router().Push("startpage");
+    return Result;
+  };
+  $mod.$resourcestrings = {strStartpage: {org: "Startseite"}};
+  $mod.$init = function () {
+    pas.dhtmlx_base.WidgetsetLoaded.then($mod.LoadStartpage);
+  };
+});
+//# sourceMappingURL=startpage.js.map
---
+++ startpage.lpr
@@ -0,0 +1,26 @@
+library startpage;
+  uses js, web, classes, Avamm, webrouter, AvammForms, dhtmlx_base,
+    dhtmlx_form,SysUtils, Types, AvammWiki;
+
+type
+
+resourcestring
+  strStartpage              = 'Startseite';
+
+Procedure DoShowStartpage(URl : String; aRoute : TRoute; Params: TStrings);
+begin
+  AvammWiki.ShowStartpage;
+end;
+
+function LoadStartpage(aValue : JSValue) : JSValue;
+begin
+  if Router.FindHTTPRoute('startpage',nil) <> nil then exit;
+  RegisterSidebarRoute(strStartpage,'startpage',@DoShowStartpage);
+  if THashHistory(Router.History).getHash='' then
+    Router.Push('startpage');
+end;
+
+initialization
+  WidgetsetLoaded._then(@LoadStartPage);
+end.
+
