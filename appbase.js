eb0ef882d37956de837d82160f641a7e596398c4
Chris <ulrich@tcs-germany.de>
2018-07-25 12:42:01 +0200

    actual submodules

--- appbase.js
+++ appbase.js
@@ -10648,7 +10648,8 @@ rtl.module("dhtmlx_base",["System","JS","Web"],function () {
       function ScriptErrorJS() {
         $mod.AppendJS("https:\/\/cdn.dhtmlx.com\/edge\/dhtmlx.js",ScriptLoadedJS,null);
         $mod.AppendCSS("https:\/\/cdn.dhtmlx.com\/edge\/dhtmlx.css",null,null);
-        $mod.AppendCSS("https:\/\/cdn.dhtmlx.com\/edge\/fonts\/font_awesome\/css\/font-awesome.min.css",null,null);
+        $mod.AppendCSS("https:\/\/use.fontawesome.com\/releases\/v5.2.0\/css\/all.css",null,null);
+        $mod.AppendCSS("https:\/\/use.fontawesome.com\/releases\/v5.2.0\/css\/v4-shims.css",null,null);
       };
       pas.System.Writeln("Loading DHTMLX...");
       $mod.AppendJS("appbase\/dhtmlx\/dhtmlx.js",ScriptLoadedJS,ScriptErrorJS);
