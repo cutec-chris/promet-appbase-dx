d8c8608ff8d8b091a23b13e0f8f0de5f1f10a426
Chris <ulrich@tcs-germany.de>
2018-07-25 11:34:22 +0200

    actual submodules

--- appbase.js
+++ appbase.js
@@ -10652,17 +10652,22 @@ rtl.module("dhtmlx_base",["System","JS","Web"],function () {
       $mod.AppendJS("appbase\/dhtmlx\/dhtmlx.js",ScriptLoadedJS,ScriptErrorJS);
     };
     function DoLoadCSS(resolve, reject) {
+      var ErrTimeout = 0;
       function ScriptLoadedCSS() {
+        window.clearTimeout(ErrTimeout);
         $mod.AppendCSS("https:\/\/cdn.dhtmlx.com\/edge\/fonts\/font_awesome\/css\/font-awesome.min.css",null,null);
         resolve(true);
       };
       function ScriptLoadedCSS2() {
+        window.clearTimeout(ErrTimeout);
         $mod.AppendCSS("appbase\/dhtmlx\/fonts\/font_awesome\/css\/font-awesome.min.css",null,null);
         resolve(true);
       };
       function ScriptErrorCSS() {
+        window.clearTimeout(ErrTimeout);
         $mod.AppendCSS("https:\/\/cdn.dhtmlx.com\/edge\/dhtmlx.css",ScriptLoadedCSS,null);
       };
+      ErrTimeout = window.setTimeout(ScriptErrorCSS,100);
       $mod.AppendCSS("appbase\/dhtmlx\/dhtmlx.css",ScriptLoadedCSS2,ScriptErrorCSS);
     };
     $mod.WidgetsetLoaded = Promise.all([new Promise(DoLoadDHTMLX),new Promise(DoLoadCSS)]);
